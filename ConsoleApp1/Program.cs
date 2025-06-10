5b9d5ccb389bb4d78317cbf892738c071380f6f4
Cédric BRASSEUR <cdric.brasseur@gmail.com>
2025-06-10 11:42:36 +0200

    Add security leaks

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Data.SqlClient;
+
+string name = "Salut";
+string password = "Monpassword^passecure";
+
+Console.WriteLine($"Password : {password}");
+
+using (SqlConnection connection = new SqlConnection(""))
+{
+    SqlCommand command = new SqlCommand($"SELECT * FROM Users WHERE name = {name} AND password = {password}", connection);
+    var test = command.ExecuteNonQuery();
+    Console.WriteLine(test);
+}
